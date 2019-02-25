8ffcafa9b8caf977a4be063258e170a16398dabf
Bayzet Tlyupov <baizet01@gmail.com>
2019-02-25 10:19:05 +0300

    Реалезовал рекурсивный подсчёт оптимальной сдачи + вывод в шаблон

--- src/Controller/MachineController.php
+++ src/Controller/MachineController.php
@@ -14,7 +14,7 @@ class MachineController extends AbstractController
 {
     // Поддерживаемые номиналы
     const MONEY = [
-        "note" => [10,50,100,500,1000], // Купюры
+        "note" => [50,100,500,1000], // Купюры
         "coin" => [1,2,5,10] // Монеты
     ];
 
@@ -45,9 +45,10 @@ class MachineController extends AbstractController
         $entityBeverage = $this->getDoctrine()->getRepository(Beverage::class)->find($id);
         $factory = new BeverageFactory();
         $beverage = $factory->createBeverage($entityBeverage);
+        $delivery = $this->payment($payDepositing - $beverage->beveragePrice);
         $result = [
             'beverage' => $beverage,
-            'delivery' => $this->payment($payDepositing, $beverage->beveragePrice)
+            'delivery' => $delivery
         ];
 
         return $this->render('machine/order.html.twig',[ 
@@ -55,9 +56,72 @@ class MachineController extends AbstractController
          ]);
     }
 
-    
-    public function payment($payDepositing, $beveragePrice){
-        return $payDepositing - $beveragePrice;
+    /**
+     * Метод расчитывает сдачу наименьшим количеством купюр и/или монет
+     * 
+     * @param int $delivery - размер сдачи
+     * @return array
+     */
+    public function payment($delivery){
+        static $result = [
+            'amount' => 0,
+            'note' => [],
+            'coin' => []
+        ];
+        static $itteration = 0;
+
+        // Если это первая иттерация рекурсии то сохраняем значение сдачи
+        if($itteration++ == 0)
+            $result['amount'] = $delivery;
+
+            
+        if($delivery != 0){
+            $tmp = [];
+            foreach($this::MONEY['note'] as $note){
+                $diff = floor($delivery/$note);
+                if($diff >= 1)
+                    $tmp['note'][$note] = $diff;
+            }
+            foreach($this::MONEY['coin'] as $coin){
+                $diff = floor($delivery/$coin);
+                if($diff >= 1)
+                    $tmp['coin'][$coin] = $diff;
+            }
+            // За счёт того, что в константе MONEY наминал расположен упорядоченно
+            // достаточно ksort и последнего элемента массива для понимания
+            ksort($tmp);
+            $tmp2 = [];
+            foreach($tmp as $key => $money){
+                // dump($value);
+                foreach($money as $key => $value){
+                    if(count($value) > 0){
+                        $tmp2[$key] = $value;
+                    }
+                }
+            }
+            ksort($tmp2);
+
+            // На этом этапе в $tmp2 всегда последний элемент самый подходящий
+            // И последний элемент всегда тот номинал которого нужно меньше всего чтобы вернуть сдачу
+            $i = 1;
+            $countTmp2 = count($tmp2);
+            $noteList = $this::MONEY['note'];
+            $coinList = $this::MONEY['coin'];
+            foreach($tmp2 as $key => $value){
+                if($i++ == $countTmp2){
+                    if(in_array($key, $noteList)){
+                        $result['note'][$key] = $value;
+                        $delivery -= $key * $value;
+                    }elseif(in_array($key, $coinList)){
+                        $result['coin'][$key] = $value;
+                        $delivery -= $key * $value;
+                    }
+                }
+            }
+            return $this->payment($delivery);
+        }else{
+            return $result;
+        }
     }
 
 }
