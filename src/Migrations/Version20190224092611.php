9591e1eec2b39df9aa72d4a8d2eb5cf7f59d1f75
Bayzet Tlyupov <baizet01@gmail.com>
2019-02-25 10:44:14 +0300

    Отладил миграции добавил небольшое описание

--- src/Migrations/Version20190224092611.php
+++ src/Migrations/Version20190224092611.php
@@ -22,7 +22,7 @@ final class Version20190224092611 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE beverage (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE beverage (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, description LONGTEXT NOT NULL, beverage_color VARCHAR(50) NOT NULL,beverage_type_id  INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
--- src/Migrations/Version20190224181035.php
+++ src/Migrations/Version20190224181035.php
@@ -23,7 +23,17 @@ final class Version20190224181035 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE beverage_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE beverage CHANGE beverage_color beverage_color VARCHAR(50) NOT NULL');
+        $this->addSql('INSERT INTO `beverage` (`id`, `name`, `price`, `description`, `beverage_color`, `beverage_type_id`) VALUES
+        (1, \'Чай чёрный\', 60, \'Чёрный чай с двумя кусочками сахара\', \'Чёрный\', 1),
+        (2, \'Чай зелёный\', 70, \'Зелёный чай без сахара\', \'Зелёный\', 1),
+        (3, \'Американо\', 100, \'50% кофе 50% вода\', \'Чёрный\', 2),
+        (4, \'Капучино\', 120, \'20% кофе 30% вода 50% молоко\', \'Серый\', 2),
+        (5, \'Тыквеный сок\', 100, \'100% тыква\', \'Оранжевый\', 3),
+        (6, \'Таматный сок\', 130, \'100% тамат\', \'Красный\', 3)');
+        $this->addSql('INSERT INTO `beverage_type` (`id`, `name`) VALUES
+            (1, \'Чай\'),
+            (2, \'Кофе\'),
+            (3, \'Сок\')');
     }
 
     public function down(Schema $schema) : void
